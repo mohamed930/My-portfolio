cb9d0cc957d7b62a7e9fef964e77097e727cec29
Mohamed Ali <mohammedali12477@gmail.com>
2020-05-09 23:50:04 +0200

    first commit

---
+++ js/java.js
@@ -0,0 +1,14 @@
+(function($){
+
+	var type = new Typed('h4.txt-rotate',{
+        strings: ["I'm Junior IOS Developer" , "I'm Junior UI/UX desgin" , "I'm Junior in Photoshop"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut : false ,
+        smartBackSpace : true,
+        loop : true 
+    });
+
+
+	
+})(jQuery);
